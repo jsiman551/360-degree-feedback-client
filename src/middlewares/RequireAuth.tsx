d3207c96ee0416abe0fb62bd3e8a27567bb12706
Jose Simancas <josesimancas51@gmail.com>
2024-11-03 23:14:20 -0500

    Fix: RequiereAuth middleware now verify token expiration

--- src/middlewares/RequireAuth.tsx
+++ src/middlewares/RequireAuth.tsx
@@ -1,22 +1,52 @@
 import React, { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../hooks/redux';
+import { jwtDecode } from 'jwt-decode';
+import { useAppSelector, useAppDispatch } from '../hooks/redux';
+import { logout } from '../redux/slices/authSlice';
+import Dashboard from '../components/dashboard';
 
 interface RequireAuthProps {
     allowedRoles?: string[];
 }
 
+interface TokenPayload {
+    exp: number;
+}
+
 const RequireAuth: React.FC<RequireAuthProps> = ({ allowedRoles = ['Admin', 'Manager', 'Employee'] }) => {
     const { token, user } = useAppSelector((state) => state.auth);
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
-        if (!token || !user || !allowedRoles.includes(user.role)) {
-            navigate('/', { replace: true });
-        }
-    }, [token, user, allowedRoles, navigate]);
+        const checkTokenExpiration = () => {
+            if (token) {
+                try {
+                    const { exp } = jwtDecode<TokenPayload>(token);
+                    const currentTime = Date.now() / 1000;
 
-    return token && user && allowedRoles.includes(user.role) ? <Outlet /> : null;
+                    // Verify if token has expired
+                    if (exp < currentTime) {
+                        dispatch(logout());
+                        navigate('/', { replace: true });
+                        return;
+                    }
+                } catch (error) {
+                    console.error('Invalid token format:', error);
+                    dispatch(logout());
+                    navigate('/', { replace: true });
+                    return;
+                }
+            } else {
+                navigate('/', { replace: true });  //If there is no token, go to login page
+            }
+        };
+
+        checkTokenExpiration();
+
+    }, [token, user, allowedRoles, navigate, dispatch]);
+
+    return token && user && allowedRoles.includes(user.role) ? <Outlet /> : <Dashboard />;
 };
 
 export default RequireAuth;
